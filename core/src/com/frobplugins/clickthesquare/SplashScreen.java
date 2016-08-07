cdb0960b84b0cb3006fa5af518b3cc2ee41341ff
FrobPlugins <public.FrobPluginsMail@gmail.com>
2016-08-07 18:51:08 +0200

    SplashScreen.java changed

--- core/src/com/frobplugins/clickthesquare/SplashScreen.java
+++ core/src/com/frobplugins/clickthesquare/SplashScreen.java
@@ -12,7 +12,7 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class SplashScreen implements Screen {
 
-    MainClass main;
+    final MainClass main;
     private Texture texture = new Texture(Gdx.files.internal("assets/Splash2.png"));
     private Image splashImage = new Image(texture);
     private Stage stage;
@@ -27,6 +27,47 @@ public class SplashScreen implements Screen {
         splashImage.setHeight(Gdx.graphics.getHeight());
         stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
         stage.addActor(splashImage);
+
+       loadMainMenu();
+    }
+
+    public void loadMainMenu(){
+        splashImage.addAction(Actions.sequence(
+                Actions.fadeOut(1f), Actions.run(new Runnable() {
+                    public void run() {
+                        Assets.loadAssets();
+                        ss = new SkinSaver();
+                        if(ss.getSkin() == 0){
+                            ss.setDefaultSkin();
+                        }
+                        if(SkinSaver.getSkin() == SkinList.gold){
+                            Assets.loadSkinGold();
+                        }
+                        if(SkinSaver.getSkin() == SkinList.green){
+                            Assets.loadSkinGreen();
+                        }
+                        if(SkinSaver.getSkin() == SkinList.light_green){
+                            Assets.loadSkinLightGreen();
+                        }
+                        if(SkinSaver.getSkin() == SkinList.metal){
+                            Assets.loadSkinMetal();
+                        }
+                        if(SkinSaver.getSkin() == SkinList.nature){
+                            Assets.loadSkinNature();
+                        }
+                        if(SkinSaver.getSkin() == SkinList.purple){
+                            Assets.loadSkinPurple();
+                        }
+                        if(SkinSaver.getSkin() == SkinList.water){
+                            Assets.loadSkinWater();
+                        }
+                        if(SkinSaver.getSkin() == SkinList.wood){
+                            Assets.loadSkinWood();
+                        }
+                        Fonts.loadFonts();
+                        ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu(main));
+                    }
+                })));
     }
 
     public void dispose() {
@@ -59,53 +100,18 @@ public class SplashScreen implements Screen {
 
     public void show() {
        // adController.hideBannerAd();
-        splashImage.addAction(Actions.sequence(Actions.alpha(0)
+        /*splashImage.addAction(Actions.sequence(Actions.alpha(0)
                 , Actions.fadeIn(1f), Actions.run(new Runnable() {
             public void run() {
                 splashImage.addAction(Actions.sequence(
                         Actions.delay(0f), Actions.run(new Runnable() {
                     public void run() {
-                        Assets.loadAssets();
-                        ss = new SkinSaver();
-                        if(ss.getSkin() == 0){
-                            ss.setDefaultSkin();
-                        }
-                        if(SkinSaver.getSkin() == SkinList.gold){
-                            Assets.loadSkinGold();
-                        }
-                        if(SkinSaver.getSkin() == SkinList.green){
-                            Assets.loadSkinGreen();
-                        }
-                        if(SkinSaver.getSkin() == SkinList.light_green){
-                            Assets.loadSkinLightGreen();
-                        }
-                        if(SkinSaver.getSkin() == SkinList.metal){
-                            Assets.loadSkinMetal();
-                        }
-                        if(SkinSaver.getSkin() == SkinList.nature){
-                            Assets.loadSkinNature();
-                        }
-                        if(SkinSaver.getSkin() == SkinList.purple){
-                            Assets.loadSkinPurple();
-                        }
-                        if(SkinSaver.getSkin() == SkinList.water){
-                            Assets.loadSkinWater();
-                        }
-                        if(SkinSaver.getSkin() == SkinList.wood){
-                            Assets.loadSkinWood();
-                        }
-                        Fonts.loadFonts();
-                        splashImage.addAction(Actions.sequence(
-                                Actions.fadeOut(1f), Actions.run(new Runnable() {
-                                    public void run() {
-                                        ((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu(main));
-                                    }
-                                })));
+
 
                     }
 
                 })));
             }
-        })));
+        })));*/
     }
 }
